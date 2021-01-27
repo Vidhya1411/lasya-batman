fcd073c4f6aff96a5e3a56d6f9fe89242fb60232
Vidhya1411 <75480858+Vidhya1411@users.noreply.github.com>
2021-01-27 20:56:01 +0530

    Add files via upload

---
+++ lasya batman/sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=100;
+
+
+
+function preload(){
+   
+}
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(400,700);
+   
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,400), random(0,400)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    background(0); 
+
+    //creating thunder
+    rand = Math.round(random(1,4));
+    
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY()
+        
+    }
+
+    drawSprites();
+}   
+
